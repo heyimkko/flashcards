1e135dec351ce9a9e57e2d99dd75e0d704c2bf1d
Apprentice <apprentice@devbootcamp.com>
2013-03-15 15:20:53 -0700

    add password authenication to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+require 'digest'
+
 class User < ActiveRecord::Base
   has_many :rounds
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.password = Digest::MD5.hexdigest(password)
+  end
+
+  def authenticate(password)
+    encrypt_password == self.password
+  end
+
 end
